7e96cc0df522eb0dd8858f333efb29ad2e3302bb
mayankchoubey <choubey@gmail.com>
2022-02-04 13:12:46 -0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -10,17 +10,14 @@ export class Tail {
     if (!fileName) {
       throw new Deno.errors.InvalidData("File path must be provided");
     }
+    Deno.statSync(fileName);
     this.fileName = fileName;
   }
 
   private async getReader(): Promise<Deno.File> {
     const fileReader = await Deno.open(this.fileName);
-    await Deno.seek(fileReader.rid, await this.getSize(), Deno.SeekMode.Start);
+    await Deno.seek(fileReader.rid, 0, Deno.SeekMode.End);
     return fileReader;
-  }
-
-  private async getSize() {
-    return (await Deno.stat(this.fileName)).size;
   }
 
   public async *start() {
@@ -30,7 +27,7 @@ export class Tail {
       if (e.kind !== "modify") {
         continue;
       }
-      for await (const line of await readStringDelim(fileReader, LF)) {
+      for await (const line of readStringDelim(fileReader, LF)) {
         if (!line) {
           break;
         }
